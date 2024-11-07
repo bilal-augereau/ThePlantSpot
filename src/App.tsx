21cd2ae4f21f33c3e0d4c3e1ddb6af32a1412510
Lassana88 <lassana2488@gmail.com>
2024-11-07 17:17:12 +0100

    modification-repository

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,92 @@
+import { useEffect, useState } from "react";
 import "./App.css";
+import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
+import Home from "../../JS-Paris-P2-ThePlantSpot/src/pages/Home.tsx";
+import Search from "../../JS-Paris-P2-ThePlantSpot/src/pages/Search.tsx";
 
 function App() {
-	const name = "toto";
-	const text = "Welcome";
+	const [plant, setPlant] = useState(null);
+	const [loading, setLoading] = useState(true);
+	const [error, setError] = useState(null);
+
+	useEffect(() => {
+		const url = "https://house-plants2.p.rapidapi.com/all";
+
+		const options = {
+			method: "GET",
+			headers: {
+				"x-rapidapi-key": "9db15f58b8msh5101d3f3e72beaep175fadjsn258b615b608e",
+				"x-rapidapi-host": "house-plants2.p.rapidapi.com",
+			},
+		};
+
+		fetch(url, options)
+			.then((response) => {
+				if (!response.ok) {
+					throw new Error(
+						"Une erreur s'est produite lors de la récupération des données",
+					);
+				}
+				return response.json();
+			})
+			.then((data) => {
+				setPlant(data);
+				setLoading(false);
+			})
+			.catch((err) => {
+				setError(err.message);
+				setLoading(false);
+			});
+	}, []);
+
+	if (loading) return <div>Chargement...</div>;
+	if (error) return <div>Erreur: {error}</div>;
+
+	console.log(plant);
 	return (
-		<>
-			{text} {name}
-		</>
+		<Router>
+			<div>
+				{/* Navigation */}
+				<nav>
+					<ul>
+						<li>
+							<Link to="/">Accueil</Link>
+						</li>
+						<li>
+							<Link to="/search">Search</Link>
+						</li>
+					</ul>
+				</nav>
+
+				{/* Définir les Routes */}
+				<Routes>
+					<Route path="/" element={<Home />} />
+					<Route path="/search" element={<Search />} />
+				</Routes>
+				<div>
+					<h1>Liste des Plantes</h1>
+					<ul>
+						{plant.map((item, index) => (
+							<li key={index}>
+								{item["Temperature max"]?.C && item["Temperature min"]?.C ? (
+									<span>
+										{item["Temperature max"].C - item["Temperature min"].C}°C
+									</span>
+								) : item["Temperature max"]?.C &&
+									!item["Temperature min"]?.C ? (
+									<span>
+										{item["Temperature max"].C}°C (Min temperature data not
+										available)
+									</span>
+								) : (
+									<span>Temperature data not available</span>
+								)}
+							</li>
+						))}
+					</ul>
+				</div>
+			</div>
+		</Router>
 	);
 }
 
