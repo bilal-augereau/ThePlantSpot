d6a69fc99c4587719fcc617efb05790bb36c669e
De Almeida Tiago <116738129+Tiagz92@users.noreply.github.com>
2024-11-15 14:16:01 +0100

    footer update

--- src/App.tsx
+++ src/App.tsx
@@ -1,71 +1,41 @@
-import { useEffect, useState } from 'react';
-import './App.css';
-import { Link, Route, BrowserRouter as Router, Routes } from 'react-router-dom';
-import Footer from './components/Footer';
-import Home from './pages/Home';
-import Search from './pages/Search';
+import "./App.css";
+import { Link, Route, BrowserRouter as Router, Routes } from "react-router-dom";
+import Footer from "./components/Footer";
+import Home from "./pages/Home";
+import Search from "./pages/Search";
 
 function App() {
-  const [plant, setPlant] = useState([]);
-  const [loading, setLoading] = useState(true);
-  const [error, setError] = useState(null);
+	return (
+		<Router>
+			<div
+				style={{
+					display: "flex",
+					flexDirection: "column",
+					minHeight: "100vh",
+				}}
+			>
+				<Link to="/">Home</Link>
+				<Link to="/search">Search</Link>
+				<Link to="/plants">Plants</Link>
 
-  useEffect(() => {
-    const url = 'https://house-plants2.p.rapidapi.com/all';
-
-    const options = {
-      method: 'GET',
-      headers: {
-        'x-rapidapi-key': '9db15f58b8msh5101d3f3e72beaep175fadjsn258b615b608e',
-        'x-rapidapi-host': 'house-plants2.p.rapidapi.com',
-      },
-    };
-
-    fetch(url, options)
-      .then((response) => {
-        if (!response.ok) {
-          throw new Error(
-            "Une erreur s'est produite lors de la récupération des données",
-          );
-        }
-        return response.json();
-      })
-      .then((data) => {
-        setPlant(data);
-        setLoading(false);
-      })
-      .catch((err) => {
-        setError(err.message);
-        setLoading(false);
-      });
-  }, []);
-
-  if (loading) return <div>Chargement...</div>;
-  if (error) return <div>Erreur: {error}</div>;
-
-  return (
-    <Router>
-      <div>
-        <nav>
-          <ul>
-            <li>
-              <Link to="/">Accueil</Link>
-            </li>
-            <li>
-              <Link to="/search">Search</Link>
-            </li>
-          </ul>
-        </nav>
-
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/search" element={<Search />} />
-        </Routes>
-
-        <Footer />
-      </div>
-    </Router>
-  );
+				<div style={{ flex: 1 }}>
+					<Routes>
+						<Route path="/" element={<Home />} />
+						<Route path="/search" element={<Search />} />
+						<Route
+							path="/plants"
+							element={
+								<div>
+									<h1>Liste des Plantes</h1>
+								</div>
+							}
+						/>
+					</Routes>
+				</div>
+				<Footer />
+			</div>
+		</Router>
+	);
 }
 
 export default App;
--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,87 +1,106 @@
-import type React from 'react';
-import logoFace from '../img/picto/Face.png';
-import logoInsta from '../img/picto/Insta.png';
-import logolink from '../img/picto/link.png';
+import type React from "react";
+import logoFace from "../img/picto/Face.png";
+import logoInsta from "../img/picto/Insta.png";
+import logolink from "../img/picto/link.png";
 
-const Footer: React.FC = () => {
-  return (
-    <footer
-      style={{
-        bottom: 0,
-        left: 0,
-        width: '100%',
-        display: 'flex',
-        justifyContent: 'space-between',
-        alignItems: 'center',
-        padding: '20px',
-        backgroundColor: '#94AB6F',
-        color: '#FFFFFF',
-        marginTop: 'auto',
-      }}
-    >
-      <div
-        style={{
-          flex: '0 1 auto',
-          textAlign: 'left',
-        }}
-      >
-        <p style={{ margin: 0 }}>© The Plant Spot</p>
-      </div>
+const Footer = () => {
+	return (
+		<footer
+			style={{
+				bottom: 0,
+				left: 0,
+				width: "100%",
+				display: "flex",
+				justifyContent: "space-between",
+				alignItems: "center",
+				padding: "20px",
+				backgroundColor: "#94AB6F",
+				color: "#FFFFFF",
+				marginTop: "auto",
+			}}
+		>
+			<div
+				style={{
+					flex: "0 1 auto",
+					textAlign: "left",
+				}}
+			>
+				<p style={{ margin: 0 }}>© The Plant Spot</p>
+			</div>
 
-      <div
-        style={{
-          flex: 1,
-          display: 'flex',
-          justifyContent: 'center',
-          gap: '20px',
-        }}
-      >
-        <img
-          src={logoInsta}
-          alt="Logo Instagram"
-          style={{ width: '40px', height: '40px' }}
-        />
-        <img
-          src={logolink}
-          alt="Logo LinkedIn"
-          style={{ width: '40px', height: '40px' }}
-        />
-        <img
-          src={logoFace}
-          alt="Logo Facebook"
-          style={{ width: '40px', height: '40px' }}
-        />
-      </div>
+			<div
+				style={{
+					flex: 1,
+					display: "flex",
+					justifyContent: "center",
+					gap: "20px",
+				}}
+			>
+				<a
+					href="https://www.instagram.com/?hl=fr"
+					target="_blank"
+					rel="noopener noreferrer"
+				>
+					<img
+						src={logoInsta}
+						alt="Logo Instagram"
+						style={{ width: "40px", height: "40px" }}
+					/>
+				</a>
+				<a
+					href="https://www.linkedin.com/"
+					target="_blank"
+					rel="noopener noreferrer"
+				>
+					<img
+						src={logolink}
+						alt="Logo LinkedIn"
+						style={{ width: "40px", height: "40px" }}
+					/>
+				</a>
+				<a
+					href="https://www.facebook.com/?locale=fr_FR"
+					target="_blank"
+					rel="noopener noreferrer"
+				>
+					<img
+						src={logoFace}
+						alt="Logo Facebook"
+						style={{ width: "40px", height: "40px" }}
+					/>
+				</a>
+			</div>
 
-      <div
-        style={{
-          flex: '0 1 auto',
-          textAlign: 'right',
-        }}
-      >
-        <a
-          href="/terms"
-          style={{
-            color: '#FFFFFF',
-            textDecoration: 'none',
-            margin: '0 10px',
-          }}
-        >
-          Terms & Conditions
-        </a>
-        <a
-          href="/privacy"
-          style={{
-            color: '#FFFFFF',
-            textDecoration: 'none',
-            margin: '0 10px',
-          }}
-        >
-          Privacy
-        </a>
-      </div>
-    </footer>
-  );
+			<div
+				style={{
+					flex: "0 1 auto",
+					textAlign: "right",
+				}}
+			>
+				<a
+					href="/terms"
+					style={{
+						width: "100%",
+						color: "#FFFFFF",
+						textDecoration: "none",
+						margin: "0 10px",
+					}}
+				>
+					Terms & Conditions
+				</a>
+				<a
+					href="/privacy"
+					style={{
+						color: "#FFFFFF",
+						textDecoration: "none",
+						margin: "0 10px",
+					}}
+				>
+					Privacy
+				</a>
+			</div>
+		</footer>
+	);
 };
 
 export default Footer;
