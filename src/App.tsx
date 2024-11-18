c461221e52c5dbf21e659302927e7ab2021b1ff0
De Almeida Tiago <116738129+Tiagz92@users.noreply.github.com>
2024-11-18 21:00:15 +0100

    footer problems resolution

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,12 @@
 import "./App.css";
 import { Link, Route, BrowserRouter as Router, Routes } from "react-router-dom";
-<<<<<<< HEAD
 import Footer from "./components/Footer";
 import Home from "./pages/Home";
 import Search from "./pages/Search";
-=======
-import Home from "../../JS-Paris-P2-ThePlantSpot/src/pages/Home.tsx";
-import Search from "../../JS-Paris-P2-ThePlantSpot/src/pages/Search.tsx";
->>>>>>> d177298129cd1e74a903b96765a47b5865b5ca06
 
 function App() {
 	return (
 		<Router>
-<<<<<<< HEAD
 			<div
 				style={{
 					display: "flex",
@@ -20,10 +14,19 @@ function App() {
 					minHeight: "100vh",
 				}}
 			>
-				<Link to="/">Home</Link>
-				<Link to="/search">Search</Link>
-				<Link to="/plants">Plants</Link>
-
+				<nav>
+					<ul>
+						<li>
+							<Link to="/">Accueil</Link>
+						</li>
+						<li>
+							<Link to="/search">Recherche</Link>
+						</li>
+						<li>
+							<Link to="/plants">Plantes</Link>
+						</li>
+					</ul>
+				</nav>
 				<div style={{ flex: 1 }}>
 					<Routes>
 						<Route path="/" element={<Home />} />
@@ -39,25 +42,6 @@ function App() {
 					</Routes>
 				</div>
 				<Footer />
-=======
-			<div>
-				{/* Navigation */}
-				<nav>
-					<ul>
-						<li>
-							<Link to="/">Accueil</Link>
-						</li>
-						<li>
-							<Link to="/search">Recherche</Link>
-						</li>
-					</ul>
-				</nav>
-				{/* Routes */}
-				<Routes>
-					<Route path="/" element={<Home />} />
-					<Route path="/search" element={<Search />} />
-				</Routes>
->>>>>>> d177298129cd1e74a903b96765a47b5865b5ca06
 			</div>
 		</Router>
 	);
