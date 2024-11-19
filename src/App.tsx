e683c625ccefe40f44f295bdd4d334d634cc392f
heyberger.augustin@gmail.com <heyberger.augustin@gmail.com>
2024-11-19 17:14:40 +0100

    oupsi (mickael)

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import "./App.css";
 import { Route, BrowserRouter as Router, Routes } from "react-router-dom";
-import Header from "./components/Header";
 import Footer from "./components/Footer";
+import Header from "./components/Header";
 import Home from "./pages/Home";
 import Search from "./pages/Search";
 
