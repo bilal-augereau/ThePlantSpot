6c4a7c68003118bc45aba443b6ea68dc8d043ba1
De Almeida Tiago <116738129+Tiagz92@users.noreply.github.com>
2024-11-19 10:19:57 +0100

    creation Footer.css

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,55 +1,46 @@
-import React from 'react';
-import './Footer.css';
-import logoFace from '../img/picto/Face.png';
-import logoInsta from '../img/picto/Insta.png';
-import logolink from '../img/picto/link.png';
+import React from "react";
+import "./Footer.css";
+import logoFace from "../img/picto/Face.png";
+import logoInsta from "../img/picto/Insta.png";
+import logolink from "../img/picto/link.png";
 
 const Footer = () => {
-    return (
-        <footer>
-            <div className="footer-left">
-                <p>© The Plant Spot</p>
-            </div>
+	return (
+		<footer>
+			<div className="footer-left">
+				<p>© The Plant Spot</p>
+			</div>
 
-            <div className="footer-center">
-                <a
-                    href="https://www.instagram.com/?hl=fr"
-                    target="_blank"
-                    rel="noopener noreferrer"
-                >
-                    <img
-                        src={logoInsta}
-                        alt="Logo Instagram"
-                    />
-                </a>
-                <a
-                    href="https://www.linkedin.com/"
-                    target="_blank"
-                    rel="noopener noreferrer"
-                >
-                    <img
-                        src={logolink}
-                        alt="Logo LinkedIn"
-                    />
-                </a>
-                <a
-                    href="https://www.facebook.com/?locale=fr_FR"
-                    target="_blank"
-                    rel="noopener noreferrer"
-                >
-                    <img
-                        src={logoFace}
-                        alt="Logo Facebook"
-                    />
-                </a>
-            </div>
+			<div className="footer-center">
+				<a
+					href="https://www.instagram.com/?hl=fr"
+					target="_blank"
+					rel="noopener noreferrer"
+				>
+					<img src={logoInsta} alt="Logo Instagram" />
+				</a>
+				<a
+					href="https://www.linkedin.com/"
+					target="_blank"
+					rel="noopener noreferrer"
+				>
+					<img src={logolink} alt="Logo LinkedIn" />
+				</a>
+				<a
+					href="https://www.facebook.com/?locale=fr_FR"
+					target="_blank"
+					rel="noopener noreferrer"
+				>
+					<img src={logoFace} alt="Logo Facebook" />
+				</a>
+			</div>
 
-            <div className="footer-right">
-                <a href="/terms">Terms & Conditions</a>
-                <a href="/privacy">Privacy</a>
-            </div>
-        </footer>
-    );
+			<div className="footer-right">
+				<a href="/terms">Terms & Conditions</a>
+				<a href="/privacy">Privacy</a>
+			</div>
+		</footer>
+	);
 };
 
 export default Footer;
