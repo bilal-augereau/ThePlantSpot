987356fdc7b1f854090fb43cc8f0f227f05ede2e
Lassana88 <lassana2488@gmail.com>
2024-11-21 17:40:01 +0100

    modification suite à la review PR concernant la 2ème partie du body

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,40 +1,44 @@
 import "./Home.css";
-import GreenOasisPicture from "../img/photo/GreenOasisPicture.jpeg";	
+import GreenOasisPicture from "../img/photo/GreenOasisPicture.jpeg";
 import IndoorPlantsPicture from "../img/photo/IndoorPlantsPicture.jpeg";
 
 const Home = () => {
 	return (
-			<div className="otherInformations">
-				<div className="exceptionalPlantVarieties">
-					<div className="text">
-						<h3>Exceptional Plant Varieties</h3>
-						<p >
-							We are here to guide you in choosing the indoor plants that suit
-							your needs. Whether it's a Kentia palm, a ficus, or succulents,
-							discover easy-to-care-for species that are perfect for air
-							purification, thrive in low light, or are safe for your pets. Our
-							mission is to help you transform your space with plants that bring
-							life, style, and serenity, all while matching your lifestyle. 🌿
-						</p>
-					</div>
-					<img className="greenOasis" src={GreenOasisPicture} alt="Green oasis" />
+		<div className="otherInformations">
+			<div className="exceptionalPlantVarieties">
+				<div className="text">
+					<h3>Exceptional Plant Varieties</h3>
+					<p>
+						We are here to guide you in choosing the indoor plants that suit
+						your needs. Whether it's a Kentia palm, a ficus, or succulents,
+						discover easy-to-care-for species that are perfect for air
+						purification, thrive in low light, or are safe for your pets. Our
+						mission is to help you transform your space with plants that bring
+						life, style, and serenity, all while matching your lifestyle. 🌿
+					</p>
 				</div>
+				<img className="greenOasis" src={GreenOasisPicture} alt="Green oasis" />
+			</div>
 
-				<div className="easyCare">
-					<img className="indoorPlants" src={IndoorPlantsPicture} alt="Indoor plants" />
-					<div className="text">
-						<h3>Easy Care</h3>
-						<p >
-							Caring for your indoor plants becomes a true pleasure with The
-							Plant Spot. We guide you step by step to understand and meet the
-							specific needs of your plants, helping them thrive to their
-							fullest potential. From choosing the right spot and watering
-							schedule to ensuring proper soil, light, and care, we provide
-							simple tips to create a lush, vibrant indoor space you’ll love.
-						</p>
-					</div>
+			<div className="easyCare">
+				<img
+					className="indoorPlants"
+					src={IndoorPlantsPicture}
+					alt="Indoor plants"
+				/>
+				<div className="text">
+					<h3>Easy Care</h3>
+					<p>
+						Caring for your indoor plants becomes a true pleasure with The Plant
+						Spot. We guide you step by step to understand and meet the specific
+						needs of your plants, helping them thrive to their fullest
+						potential. From choosing the right spot and watering schedule to
+						ensuring proper soil, light, and care, we provide simple tips to
+						create a lush, vibrant indoor space you’ll love.
+					</p>
 				</div>
 			</div>
+		</div>
 	);
 };
 
