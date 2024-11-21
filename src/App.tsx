01685c082f0ac8041a39756eaba203fdce9d39e7
De Almeida Tiago <116738129+Tiagz92@users.noreply.github.com>
2024-11-21 16:19:27 +0100

    home first paragraph & quiz button update

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,4 @@
 import "./App.css";
-import "./Home.css";
 import { Route, BrowserRouter as Router, Routes } from "react-router-dom";
 import Footer from "./components/Footer";
 import Header from "./components/Header";
