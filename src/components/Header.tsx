a383294778ebc52623bea3ec974ec98ab88beca6
heyberger.augustin@gmail.com <heyberger.augustin@gmail.com>
2024-11-25 16:39:32 +0100

    header_SwapColor

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,46 +1,27 @@
-import { useCallback, useEffect, useState } from "react";
+import { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
 import logo from "../img/illustrations/Logo.png";
 import "./Header.css";
 
 const Header = () => {
-	const [navbarColor, setNavbarColor] = useState("#94ab6F");
-
-	const interpolateColor = useCallback(
-		(color1: string, color2: string, factor: number): string => {
-			const c1 = color1
-				.match(/\w\w/g)
-				?.map((hex) => Number.parseInt(hex, 16)) || [0, 0, 0];
-			const c2 = color2
-				.match(/\w\w/g)
-				?.map((hex) => Number.parseInt(hex, 16)) || [0, 0, 0];
-			const result = c1.map((start, i) =>
-				Math.round(start + (c2[i] - start) * factor),
-			);
-
-			return `rgb(${result[0]}, ${result[1]}, ${result[2]})`;
-		},
-		[],
-	);
+	const [navSwapColor, setNavSwapColor] = useState("#435823");
 
 	useEffect(() => {
 		const handleScroll = () => {
-			const scrollY = window.scrollY;
-			const maxScroll = 1000;
-			const factor = Math.min(scrollY / maxScroll, 1);
-			const newColor = interpolateColor("#94ab6f", "#435823", factor);
-			setNavbarColor(newColor);
+			if (window.scrollY > 60) {
+				setNavSwapColor("#435823");
+			} else {
+				setNavSwapColor("#94ab6f");
+			}
 		};
-
 		window.addEventListener("scroll", handleScroll);
-
 		return () => {
 			window.removeEventListener("scroll", handleScroll);
 		};
-	}, [interpolateColor]);
+	});
 
 	return (
-		<nav style={{ backgroundColor: navbarColor }}>
+		<nav style={{ backgroundColor: navSwapColor }}>
 			<div className="column-logo">
 				<img src={logo} alt="logo" className="Logo" />
 
